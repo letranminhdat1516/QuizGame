6a28f6da069a0c6a57222aeaee1736dc9994ebe3
NguyenHP <hoangtronghuy.KT@gmail.com>
2025-04-02 01:52:19 +0700

    fix

--- Asm_PRN222_QuizGame/QuizGame.Player/obj/Debug/net7.0/QuizGame.Player.AssemblyInfo.cs
+++ Asm_PRN222_QuizGame/QuizGame.Player/obj/Debug/net7.0/QuizGame.Player.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizGame.Player")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4caaa60d858a1e122da008932d540a8e0ac8fe50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76f346d4300cf2100cbc74138903aa89f6bc8bf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizGame.Player")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizGame.Player")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
